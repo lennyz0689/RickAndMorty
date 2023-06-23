69965b884a77e7859ecdd6918896b91ce2c96281
lennyz0689 <lennyz0689@gmail.com>
2023-06-23 10:04:11 -0500

    Creacion de la funcion onSerach para agregar personajes al estado

--- src/App.js
+++ src/App.js
@@ -4,9 +4,26 @@ import Cards from './components/Cards/Cards.jsx';
 import Nav from './components/Nav/Nav';
 import { useState } from 'react';
 
+const example = {
+   id: 1,
+   name: 'Rick Sanchez',
+   status: 'Alive',
+   species: 'Human',
+   gender: 'Male',
+   origin: {
+      name: 'Earth (C-137)',
+      url: 'https://rickandmortyapi.com/api/location/1',
+   },
+   image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+};
+
 function App() {
 
    const [characters, setcharacters] = useState([])
+
+   const onSearch = () =>{
+      setcharacters([...characters, example])
+   }
 
    return (
       <div className='App'>
