7ba07dce44e580c5d87c26dfa0e487cecdeabe04
lennyz0689 <lennyz0689@gmail.com>
2023-06-23 09:52:36 -0500

    Creacion del estado

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import './App.css';
 // import Card from './components/Card/Card';
 import Cards from './components/Cards/Cards.jsx';
 import Nav from './components/Nav/Nav';
-import characters, {Rick}  from './data.js';
+import { useState } from 'react';
 
 function App() {
+
+   const [characters, setcharacters] = useState([])
+
    return (
       <div className='App'>
          <Nav/>
